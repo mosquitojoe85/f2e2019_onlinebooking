f911678ec48b180cf86a260497af8d77c4f979f5
Unknown <mosquitojoe85@gmail.com>
2019-08-26 00:01:01 +0800

    [ADD] 增加 DEMO 檔案

---
+++ build/precache-manifest.b55166810fdb3345595df625d3f5255e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b78a7529b4e27b8e5e0aac43d46a230f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "829e0ba2a7dac1551f48",
+    "url": "./static/css/main.f83694bc.chunk.css"
+  },
+  {
+    "revision": "a10c24486bcefeb49b5b",
+    "url": "./static/js/2.f9924a05.chunk.js"
+  },
+  {
+    "revision": "829e0ba2a7dac1551f48",
+    "url": "./static/js/main.9070cf38.chunk.js"
+  },
+  {
+    "revision": "c96e8d9fc65fe8f7bb98",
+    "url": "./static/js/runtime~main.4adacd0a.js"
+  },
+  {
+    "revision": "adb907afab4695da74c12928d996361c",
+    "url": "./static/media/01.adb907af.jpg"
+  },
+  {
+    "revision": "4dc067fdf32f5963ef34ea2c5c5ec13b",
+    "url": "./static/media/02.4dc067fd.jpg"
+  },
+  {
+    "revision": "89018568815a95ba1cb274211b655b90",
+    "url": "./static/media/03.89018568.jpg"
+  },
+  {
+    "revision": "640c83f56512295df63093ba4f1c8f09",
+    "url": "./static/media/04.640c83f5.jpg"
+  },
+  {
+    "revision": "3fafe7076ef3b04d64a9e3819ec2d7e9",
+    "url": "./static/media/logo.3fafe707.svg"
+  },
+  {
+    "revision": "805ea723217be10aa611b530402cf565",
+    "url": "./static/media/photo1.805ea723.png"
+  },
+  {
+    "revision": "803089b2b2eb203f7a8a19d19a8f775c",
+    "url": "./static/media/photo2.803089b2.png"
+  },
+  {
+    "revision": "0c119ef632ea6679b5f01669ef24f9de",
+    "url": "./static/media/photo3.0c119ef6.png"
+  },
+  {
+    "revision": "b66da979c63863f6e7b2d49cdf739928",
+    "url": "./static/media/photo4.b66da979.png"
+  },
+  {
+    "revision": "2dbddf5fbcd8ee814309e1c5bf77b429",
+    "url": "./static/media/photo5.2dbddf5f.png"
+  },
+  {
+    "revision": "4558f5bc487b6c00834e39f5a57c20ce",
+    "url": "./static/media/photo6.4558f5bc.png"
+  }
+]);
